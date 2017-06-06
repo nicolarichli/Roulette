b57f202b981944c4210546c3e5dd73c62214359d
Töbu <Tbu@Trpfli>
2017-06-06 15:18:20 +0200

    GameControl

--- Roulette/src/LOGIC/GameControl.java
+++ Roulette/src/LOGIC/GameControl.java
@@ -6,8 +6,8 @@ import DATA.*;
 public class GameControl{
 	Wheel wheel = new Wheel();
 	DataTransmission dataTransmission = new DataTransmission();
-	ArrayList fields = new ArrayList();
-	ArrayList settedChips = new ArrayList();
+	ArrayList<Field> fields = new ArrayList<Field>();
+	ArrayList<Chip> settedChips = new ArrayList<Chip>();
 	
 	public void chipOnField(){
 		
@@ -34,26 +34,43 @@ public class GameControl{
 	}
 	
 	NumberField n0 = new NumberField();
-	NumberField n0 = new NumberField();
-	NumberField n0 = new NumberField();
-	NumberField n0 = new NumberField();
-	NumberField n0 = new NumberField();
-	NumberField n0 = new NumberField();
-	NumberField n0 = new NumberField();
-	NumberField n0 = new NumberField();
-	NumberField n0 = new NumberField();
-	NumberField n0 = new NumberField();
-	NumberField n0 = new NumberField();
-	NumberField n0 = new NumberField();
-	NumberField n0 = new NumberField();
-	NumberField n0 = new NumberField();
-	NumberField n0 = new NumberField();
-	NumberField n0 = new NumberField();
-	NumberField n0 = new NumberField();
-	NumberField n0 = new NumberField();
-	NumberField n0 = new NumberField();
-	NumberField n0 = new NumberField();
-	NumberField n0 = new NumberField();
-	NumberField n0 = new NumberField();
-	NumberField n0 = new NumberField();
+	NumberField n1 = new NumberField();
+	NumberField n2 = new NumberField();
+	NumberField n3 = new NumberField();
+	NumberField n4 = new NumberField();
+	NumberField n5 = new NumberField();
+	NumberField n6 = new NumberField();
+	NumberField n7 = new NumberField();
+	NumberField n8 = new NumberField();
+	NumberField n9 = new NumberField();
+	NumberField n10 = new NumberField();
+	NumberField n11 = new NumberField();
+	NumberField n12 = new NumberField();
+	NumberField n13 = new NumberField();
+	NumberField n14 = new NumberField();
+	NumberField n15 = new NumberField();
+	NumberField n16 = new NumberField();
+	NumberField n17 = new NumberField();
+	NumberField n18 = new NumberField();
+	NumberField n19 = new NumberField();
+	NumberField n20 = new NumberField();
+	NumberField n21 = new NumberField();
+	NumberField n22 = new NumberField();
+	NumberField n23 = new NumberField();
+	NumberField n24 = new NumberField();
+	NumberField n25 = new NumberField();
+	NumberField n26 = new NumberField();
+	NumberField n27 = new NumberField();
+	NumberField n28 = new NumberField();
+	NumberField n29 = new NumberField();
+	NumberField n30 = new NumberField();
+	NumberField n31 = new NumberField();
+	NumberField n32 = new NumberField();
+	NumberField n33 = new NumberField();
+	NumberField n34 = new NumberField();
+	NumberField n35 = new NumberField();
+	NumberField n36 = new NumberField();
+	
+	
+	
 }
